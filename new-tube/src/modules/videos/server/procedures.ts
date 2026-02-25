d53e0ea3b9e46823b8909a957feab5ac1bdeb195
Athibodee Keawlaithong <athibodee.kaewlaithong@sony.com>
2026-02-25 14:01:21 +0700

    [User page] feat: implement user profile and video sections, add user banner and info components, enhance user data fetching with subscriptions and videos

--- new-tube/src/modules/videos/server/procedures.ts
+++ new-tube/src/modules/videos/server/procedures.ts
@@ -199,6 +199,7 @@ export const videosRouter = createTRPCRouter({
     .input(
       z.object({
         categoryId: z.string().uuid().nullish(),
+        userId: z.string().uuid().nullish(),
         cursor: z
           .object({
             id: z.string().uuid(),
@@ -209,7 +210,7 @@ export const videosRouter = createTRPCRouter({
       }),
     )
     .query(async ({ input }) => {
-      const { cursor, limit, categoryId } = input;
+      const { cursor, limit, categoryId, userId } = input;
 
       const data = await db
         .select({
@@ -237,6 +238,7 @@ export const videosRouter = createTRPCRouter({
           and(
             eq(videos.visibility, "public"),
             categoryId ? eq(videos.categoryId, categoryId) : undefined,
+            userId ? eq(videos.userId, userId) : undefined,
             cursor
               ? or(
                   lt(videos.updatedAt, cursor.updatedAt),
